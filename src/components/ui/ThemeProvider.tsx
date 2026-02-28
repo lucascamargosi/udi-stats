36a516b4af1f888d2233dc0f287a7b5db0b04725
lucas <lucas.camargos@outlook.com>
2026-02-28 11:40:02 -0300

    feat(theme): implementa tema claro/escuro com ThemeProvider, ThemeToggle e ajustes no global.css

---
+++ src/components/ui/ThemeProvider.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { ThemeProvider as NextThemeProvider } from "next-themes"
+import React from "react"
+
+export default function ThemeProvider({ children }: { children: React.ReactNode }) {
+    return (
+        <NextThemeProvider attribute='class' defaultTheme="system" enableSystem>
+            {children}
+        </NextThemeProvider>
+    )
+}
